b31b5c1f5efa95f66b14fd9a77e45510fb4679b7
spike014 <l1ng14@foxmail.com>
2024-09-02 00:31:40 +0800

    feat: add react

--- react/my-app/src/App.js
+++ react/my-app/src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
+import { createContext, useContext, useState } from 'react';
 import './App.css';
+
+const context = createContext('')
+
+function Son() {
+  const name = 'name from A'
+  return <div>
+          {name}
+          <div><SonB></SonB></div>
+         </div>
+}
+
+function SonB() {
+  const c = useContext(context)
+  return <div>This is B. I got `{c}`</div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is a react app.
+      <br />
+
+      <context.Provider value='message from App by context'>
+        <Son >
+          <span>This is a span</span>
+        </Son>
+      </context.Provider>
     </div>
   );
 }
